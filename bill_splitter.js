907b62d115420970e7f7c695fed9f3b5d928a07e
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-07 12:20:27 -0400

    Add output functionality to display bill, tip, and total

--- bill_splitter.js
+++ bill_splitter.js
@@ -2,3 +2,7 @@
 
 var bill_size =163;
 var tip = bill_size >= 50 && bill_size <= 300 ? bill_size*0.15 : bill_size*0.20; //identify the bill size and check in what "tip bracket" does that bill fits in.
+
+//Task 2-Output Details
+
+console.log(`The bill was $${bill_size}, the tip was $${tip}, and the total value is $${bill_size + tip}`); //Display a message in the console saying the bill amount, the tip amount and how much was the total value when the bill and the tip are added up.
